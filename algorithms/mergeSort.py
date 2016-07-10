6f085a1c172715234ea32daabc50df6f739833bb
MariiaKravtsova <kravtsova.mariia@gmail.com>
2016-07-09 22:08:24 -0400

    More merge sort that doesn't work

--- algorithms/mergeSort.py
+++ algorithms/mergeSort.py
@@ -1,28 +1,38 @@
-def mergeSort(array):
-    """Implement merge sort"""
-    if len(array) < 1:
+def divide(array):
+    """Divide the array"""
+    length = int(len(array))
+    if length > 1:
+        mid = int(length / 2)
+        left = array[:mid]
+        right = array[mid:]
+        divide(left)
+        divide(right)
+        # print(left, right)
+        return conquer(left, right)
+    else:
         return array
-    mid = int(len(array) / 2)
-    left = mergeSort(array[:mid])
-    right = mergeSort(array[mid:])
-    # mergeSort(left)
-    # mergeSort(right)
 
-    i = 0
-    j = 0
-
+def conquer(left, right):
+    """Sort and merge"""
     sortedArray = []
-
-    while i < len(left) and j < len(right):
+    i, j = 0, 0
+    while (len(left) > i) and (len(right) > j):
         if left[i] < right[j]:
             sortedArray.append(left[i])
+            print(left[i], right[j])
+            print("Sorted ", sortedArray)
             i = i + 1
         else:
             sortedArray.append(right[j])
+            print(left[i], right[j])
+            print("Sorted ", sortedArray)
             j = j + 1
-    sortedArray = sortedArray + left[i:]
-    sortedArray = sortedArray + right[:i]
+
+    sortedArray += left[i:]
+    sortedArray += right[j:]
+
     print(sortedArray)
+    return sortedArray
 
 myArray = [1, 3, 8, 2, 5, 0]
-mergeSort(myArray)
+print(divide(myArray))
