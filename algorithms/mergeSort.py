20c98a76dcc0b1cec9ab4fbed69113cc762aa1f4
MariiaKravtsova <kravtsova.mariia@gmail.com>
2016-07-10 15:01:03 -0400

    Found the error: I wasn't saving the recursive list.

--- algorithms/mergeSort.py
+++ algorithms/mergeSort.py
@@ -1,38 +1,37 @@
 def divide(array):
     """Divide the array"""
     length = int(len(array))
-    if length > 1:
+    if length == 0 or length == 1:
+        return array
+    else:
         mid = int(length / 2)
         left = array[:mid]
         right = array[mid:]
-        divide(left)
-        divide(right)
-        # print(left, right)
+        left = divide(left)
+        right = divide(right)
+        print(left, right)
         return conquer(left, right)
-    else:
-        return array
 
 def conquer(left, right):
     """Sort and merge"""
     sortedArray = []
     i, j = 0, 0
-    while (len(left) > i) and (len(right) > j):
+    while (i < len(left)) and (j < len(right)):
         if left[i] < right[j]:
             sortedArray.append(left[i])
-            print(left[i], right[j])
-            print("Sorted ", sortedArray)
-            i = i + 1
+            i += 1
         else:
             sortedArray.append(right[j])
-            print(left[i], right[j])
-            print("Sorted ", sortedArray)
-            j = j + 1
+            j += 1
 
-    sortedArray += left[i:]
-    sortedArray += right[j:]
+    while i < len(left):
+        sortedArray.append(left[i])
+        i += 1
+    while j < len(right):
+        sortedArray.append(right[j])
+        j += 1
 
-    print(sortedArray)
-    return sortedArray
+    return(sortedArray)
 
 myArray = [1, 3, 8, 2, 5, 0]
 print(divide(myArray))
