c060f8e51f51173da96714ead85ca1535c3ef472
MariiaKravtsova <kravtsova.mariia@gmail.com>
2016-07-20 22:38:56 -0400

    Solve word count

--- Exercism/word-count/wordcount.py
+++ Exercism/word-count/wordcount.py
@@ -2,7 +2,8 @@ import re, collections
 
 def word_count(sentence):
     """Count the words in a sentence"""
-
-    words = re.split('[\s^a-zA-Z0-9-]', sentence)
+    words = re.sub(r'[^а-яА-ЯA-Za-z0-9]', " ", sentence.lower())
+    words = words.split()
+    result = {}
     result = collections.Counter(words)
     return result
